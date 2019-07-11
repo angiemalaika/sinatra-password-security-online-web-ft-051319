ae2b45d9b026eff9ba8505ac3c872b5fe6cb68a4
Angela Mumba <malaika.mumba@gmail.com>
2019-07-11 05:49:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,12 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
-  user = User.find_by(:username => params[:username])
-		if user && user.authenticate(params[:password])
-			session[:user_id] = user.id
-			redirect "/success"
+  	user = User.new(:username => params[:username],:password => params[:username])
+  	
+  	if user.save
+			redirect "/login"
 		else
-			redirect "failure"
+			redirect "/failure"
 		end
   end
 
